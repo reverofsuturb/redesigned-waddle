58c8b87c6410f8438e859cee47b1842b2407a2ad
sam <sfriedman6711@gmail.com>
2024-06-07 23:57:58 -0600

    front-matter added

--- chatbot-scripts/index.js
+++ chatbot-scripts/index.js
@@ -1,9 +1,11 @@
-const { parse } = require("@babel/core");
+import fm from "front-matter";
 
 const parseExpoDocs = async (slug) => {
   const url = `https://raw.githubusercontent.com/expo/main/docs/pages/${slug}.mdx`;
   const response = await fetch(url);
   const content = await response.text();
+
+  const data = fm(content);
   console.log(content);
 };
 
